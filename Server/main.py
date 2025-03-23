e0a2914738ecbaac30bf855d9939c1c8d62a81a9
Roman <Roman.Karmatsky@solisoft.group>
2025-03-23 22:40:24 +0300

    Added pay and init methods

--- Server/main.py
+++ Server/main.py
@@ -1,5 +1,25 @@
-from models import Owner, Room, Repaiment, init_storage
+from datetime import strptime
+from models import db, Room, Repaiment, init_storage
+from flask import request
 from application import app
+
+
+@app.route("/init", methods=['POST'])
+def init():
+    init_storage()
+    return "Storage initialized"
+
+@app.route("/repaiments", methods=['POST'])
+def set_repaiments():
+    data = request.json
+    room_id = int(data.get('room_id'))
+    owner_id = int(data.get('owner_id'))
+    Repaiment.filter_by(room=room_id, owner=owner_id).delete()
+    for repaiment in data.get('repaiments'):
+        db.session.add(Repaiment(room=room_id, owner=owner_id, amount=0, target=repaiment['target'], date=strptime(repaiment['date'], '%Y-%m')))
+    
+    db.session.commit()
+    return "Storage initialized"
 
 @app.route("/room/<int:room_id>", methods=['GET'])
 def get_parts(room_id):
@@ -7,21 +27,27 @@ def get_parts(room_id):
     if room is None:
         return "Room not found", 404
     
-    output = "Owners:<br>"
+    output = { "id": room.id, "price": room.price, "owners": [], "repaiments": [] }
     for owner in room.owners:
-        output += f"{owner.name} owns this room. "
+        output["owners"].append({"name": owner.name, "part": next((item.part for item in owner.parts if item.room == room_id), None) })
 
-    output += "<br>Repaiments:<br>"
     for repaiment in room.repaiments:
-        output += f"Repaiment: {repaiment.amount} for {repaiment.target}. "
-
-    output += "<br>"
+        output["repaiments"].append({"amount": repaiment.amount, "target": repaiment.target })
     
     return output
 
+@app.route("/pay", methods=['POST'])
 def pay():
-    pass
+    data = request.json
+    room_id = int(data.get('room_id'))
+    owner_id = int(data.get('owner_id'))
+    date = strptime(data.get('date'), '%Y-%m')
+    retaiment = Repaiment.query.order_by(Repaiment.date.asc()).filter_by(room=room_id, owner=owner_id, date = date).first()
+    if retaiment is None:
+        return "No repaiments found", 404
+    
+    retaiment.amount += int(data.get('amount'))
+    db.session.commit()
 
 if __name__ == '__main__':
-    init_storage()
     app.run(port=3000, debug=True)
